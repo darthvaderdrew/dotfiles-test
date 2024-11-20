e558187235e4c834ccdda9d776357ca5476b051e
drew <darthvaderdrew@gmail.com>
2024-11-20 10:07:14 -0600

    refactor(nvim): rename ibl plugin file, add lazy loading, and exclude specific filetypes

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,18 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   ---@module "ibl"
   ---@type ibl.config
   opts = {
     indent = { char = "┊" },
     scope = { enabled = true },
+    exclude = {
+      filetypes = {
+        "help",
+        "lazy",
+        "mason",
+      },
+    },
   },
 }
