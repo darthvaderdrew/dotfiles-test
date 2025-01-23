c3b9367223129940bcaf64efe7fb64b7fc241fd3
drew <darthvaderdrew@gmail.com>
2025-01-23 11:38:43 -0600

    chore: update plugins, paths, and submodules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 require("config.lazy") -- lazy package manager and all plugins
-require("config.options") -- all neovim options
+require("config.options") -- neovim options
 require("config.keymaps") -- custom keymaps
 require("config.functions") -- custom functions
 require("config.autocmds") -- custom autocommands
