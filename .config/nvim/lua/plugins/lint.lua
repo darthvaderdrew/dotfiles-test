bb6d0820593c74231d89ac9c1d146a01faec0539
drew <darthvaderdrew@gmail.com>
2025-02-12 13:19:48 -0600

    feat(nvim): add nvim-lint plugin

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,20 @@
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      sql = { "sqlfluff" },
+    },
+
+    vim.api.nvim_create_autocmd({
+      "BufEnter",
+      "BufWritePost",
+      "InsertLeave",
+    },
+      {
+      pattern = "*",
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end,
+}
