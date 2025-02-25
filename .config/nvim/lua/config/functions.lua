1a6ac09c44c7b138fece3938df07f6f34b65f3fb
drew <darthvaderdrew@gmail.com>
2025-02-25 16:19:33 -0600

    feat(nvim): add custom function to toggle diagnostics display

--- .config/nvim/lua/config/functions.lua
+++ .config/nvim/lua/config/functions.lua
@@ -10,5 +10,15 @@ function M.trim_whitespace()
 end
 vim.api.nvim_create_user_command("TrimWhitespace", M.trim_whitespace, {})
 
+-- toggle diagnostics display
+function M.toggle_diagnostics()
+    local current_state = vim.diagnostic.config().virtual_text
+    vim.diagnostic.config({
+        virtual_text = not current_state,
+        signs = not current_state,
+        underline = not current_state,
+    })
+end
+vim.api.nvim_create_user_command("ToggleDiagnostics", M.toggle_diagnostics, {})
 
 return M
