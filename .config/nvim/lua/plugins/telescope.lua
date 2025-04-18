30c69b4dba269bbebc9433b84488e731b9151015
drew <darthvaderdrew@gmail.com>
2025-04-17 21:36:23 -0500

    chore(nvim): show diagnostics only for the current buffer in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
+    vim.keymap.set("n", "<leader>fd", function() builtin.diagnostics({ bufnr = 0}) end, { desc = "Telescope diagnostics" })
     vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Telescope treesitter" })
 
     -- telescope options
